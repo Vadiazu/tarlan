58fa2bc5426d3e14e6d8a5b560e7d5dbba415ce5
alexey <hrmZSuuMFBXW5o>
2013-12-20 23:43:18 +0200

    footer in progress

--- js/frontend.js
+++ js/frontend.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
     $(".placeholder-container label").inFieldLabels();
-})
+
+    //init main page photo vs video slider
+    $(".photo-video-slider-start").bxSlider({
+        auto: true,
+        pause: 8000,
+        slideWidth: 172,
+        slideMargin: 35,
+        minSlides: 5,
+        maxSlides: 7,
+        moveSlides: 1,
+        pager: false
+    });
+
+    $(".partners-slider").bxSlider({
+        auto: true,
+        pause: 8000,
+        slideWidth: 135,
+        slideMargin: 10,
+        minSlides: 5,
+        maxSlides: 12,
+        moveSlides: 1,
+        pager: false,
+        controls: false
+    });
+
+});
