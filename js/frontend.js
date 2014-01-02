b10a47641143c7220b6fe40d64f31c394431ffa7
Vadiazu <victor.pristupa@gmail.com>
2014-01-02 21:15:31 +0200

    center blocks

--- js/frontend.js
+++ js/frontend.js
@@ -108,49 +108,72 @@ $(document).ready(function(){
     }
 
     //works calendar slider
-    $(".competitions-slider-start ").bxSlider({
-        auto: false,
-        pause: 10000,
-        slideWidth: 235,
-        slideMargin: 20,
-        minSlides: 5,
-        maxSlides: 5,
-        moveSlides: 1,
-        pager: false,
-        controls: true
-    });
+    if($(".competitions-slider-start").length){
+        $(".competitions-slider-start").bxSlider({
+            auto: false,
+            pause: 10000,
+            slideWidth: 235,
+            slideMargin: 20,
+            minSlides: 5,
+            maxSlides: 5,
+            moveSlides: 1,
+            pager: false,
+            controls: true
+        });
+    }
 
-   var customSlider = function(name){
-       var $frame = $(name);
-       var $wrap  = $frame.parent();
 
-       // Call Sly on frame
-       $frame.sly({
-           horizontal: 1,
-           itemNav: 'forceCentered',
-           smart: 1,
-           activateMiddle: 1,
-           activateOn: 'click',
-           mouseDragging: 0,
-           touchDragging: 0,
-           releaseSwing: 1,
-           startAt: 0,
-           scrollBar: $wrap.find('.scrollbar'),
-           scrollBy: 1,
-           speed: 500,
-           elasticBounds: 1,
-           dragHandle: 1,
-           dynamicHandle: 1,
-           clickBar: 1,
+    if($(".winners-slide-block").length){
+        var customSlider = function(name){
+            var $frame = $(name);
+            var $slidee = $frame.children('ul').eq(0);
+            var $wrap  = $frame.parent();
+            console.log($wrap);
+            // Call Sly on frame
+            $frame.sly({
+//                horizontal: 1,
+//                itemNav: 'basic',
+//                smart: 1,
+//                activateOn: 'click',
+//                mouseDragging: 1,
+//                touchDragging: 1,
+//                releaseSwing: 1,
+//                startAt: 0,
+//                scrollBar: $wrap.find('.scrollbar'),
+//                scrollBy: 1,
+//                speed: 500,
+//                elasticBounds: 1,
+//                dragHandle: 1,
+//                dynamicHandle:  1,
+//                minHandleSize: 420,
+//                clickBar: 1,
 
-           // Buttons
-           prev: $wrap.find('.btn-prev'),
-           next: $wrap.find('.btn-next')
-       });
-   };
+                horizontal: 1,
+                itemNav: 'basic',
+                smart: 1,
+                activateOn: 'click',
+                mouseDragging: 1,
+                touchDragging: 1,
+                releaseSwing: 1,
+                startAt: 0,
+                scrollBar: $wrap.find('.scrollbar'),
+                scrollBy: 1,
+                activatePageOn: 'click',
+                speed: 300,
+                elasticBounds: 1,
+                dragHandle: 1,
+                dynamicHandle: 1,
+                clickBar: 1,
 
-    customSlider("#winnerTab1 .custom-slider");
-    customSlider("#winnerTab2 .custom-slider");
+                // Buttons
+                prev: $wrap.find('.btn-prev'),
+                next: $wrap.find('.btn-next')
+            });
+        };
+
+        customSlider("#winnerTab1 .custom-slider");
+        customSlider("#winnerTab2 .custom-slider");
+    }
 
     /*Tabs*/
     var tabsFunc = function(containerName, tabsBtn){
